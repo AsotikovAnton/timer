9474ae182baa0488a00c0616a7f8357d824f33bb
Asotikov Anton <asant@yandex.ru>
2020-10-25 13:04:07 +0300

    upd

--- src/timer.js
+++ src/timer.js
@@ -1,41 +1,44 @@
-const audio = document.getElementById('audio');
+import '../src/style.css';
 
 export const timerVision = () => {
-  const timer = document.querySelector('.timer');
-  const input = document.querySelector('.time-input');
-  const playBtn = document.getElementById('button-addon2');
-  
-  timer.textContent = 60;
-  input.value = 60;
-  
-  const playTimer = () => {
-    timer.textContent = input.value;
-    input.disabled = true;
-    playBtn.disabled = true;
-  
-    let timerInterval = setInterval(() => {
-      if (timer.textContent > 0) {
-        timer.textContent --;
-      }
-  
-      if (timer.textContent == Math.ceil(input.value / 2)) {
-        playAudio();
-      }
-  
-      if (timer.textContent < 1) {
-        playAudio();
-        clearInterval(timerInterval);
-        input.disabled = false;
-        playBtn.disabled = false;
-      }
-  
-    }, 1000);
-  }
-  
-  playBtn.addEventListener('click', playTimer);
-}
-
-const playAudio = () => {
-  audio.play();
-  audio.currentTime = 0.0;
+  document.addEventListener("DOMContentLoaded", () => {
+    const audio = document.getElementById('audio');
+    const timer = document.querySelector('.timer');
+    const input = document.querySelector('.time-input');
+    const playBtn = document.getElementById('button-addon2');
+    
+    timer.textContent = 60;
+    input.value = 60;
+    
+    const playTimer = () => {
+      timer.textContent = input.value;
+      input.disabled = true;
+      playBtn.disabled = true;
+    
+      let timerInterval = setInterval(() => {
+        if (timer.textContent > 0) {
+          timer.textContent --;
+        }
+    
+        if (timer.textContent == Math.ceil(input.value / 2)) {
+          playAudio();
+        }
+    
+        if (timer.textContent < 1) {
+          playAudio();
+          clearInterval(timerInterval);
+          input.disabled = false;
+          playBtn.disabled = false;
+        }
+    
+      }, 1000);
+    }
+    
+    const playAudio = () => {
+      audio.play();
+      audio.currentTime = 0.0;
+    }
+    
+    playBtn.addEventListener('click', playTimer);
+  })
 }
