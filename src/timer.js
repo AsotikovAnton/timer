7fc893b4758effe292eff32e7451d60be6f7a2ea
Asotikov Anton <asant@yandex.ru>
2020-10-31 15:30:20 +0300

    upd

--- src/timer.js
+++ src/timer.js
@@ -4,44 +4,43 @@ export const timerVision = () => {
   const timer = document.querySelector('.timer');
   const input = document.querySelector('.time-input');
   const playBtn = document.getElementById('button-addon2');
-  
+
   timer.textContent = 60;
   input.value = 60;
-  
+
   const playTimer = () => {
     playAudioStart();
     timer.textContent = input.value;
     input.disabled = true;
     playBtn.disabled = true;
-  
+
     let timerInterval = setInterval(() => {
       if (timer.textContent > 0) {
-        timer.textContent --;
+        timer.textContent--;
       }
-  
+
       if (timer.textContent == Math.ceil(input.value / 2)) {
         playAudio();
       }
-  
+
       if (timer.textContent < 1) {
         playAudio();
         clearInterval(timerInterval);
         input.disabled = false;
         playBtn.disabled = false;
       }
-  
     }, 1000);
-  }
-  
+  };
+
   const playAudio = () => {
     audio.play();
     audio.currentTime = 0.0;
-  }
+  };
 
   const playAudioStart = () => {
     audioStart.play();
     audioStart.currentTime = 0.0;
-  }
-  
+  };
+
   playBtn.addEventListener('click', playTimer);
-}
+};
